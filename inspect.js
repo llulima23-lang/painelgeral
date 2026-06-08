fd398631b965f4d9046bfeb04afeb0fa13c02127
llulima23-lang <llu.lima23@gmail.com>
2026-06-08 18:22:39 -0300

    Delete inspect.js

--- inspect.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('C:/Users/sup.luciana/Desktop/AntiGravity/PAINEL GERAL/data_v2.js', 'utf8');
-const match = content.match(/meta2024['"]?\s*:\s*\[([\s\S]*?)\]/);
-if (match) console.log(match[0].substring(0, 500));
-else console.log('not found');
